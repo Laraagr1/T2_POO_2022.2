eeecc2b80edc7507d83d42b4b22ad73af6b3fb05
Erick Gomes <110913912+ErickGms01@users.noreply.github.com>
2022-09-22 09:56:21 -0300

    Adicionando IF's

--- 02 - Opções/src/main/Main.java
+++ 02 - Opções/src/main/Main.java
@@ -6,16 +6,16 @@ public class Main {
 	
 	public static void main(String[] args) {
 		int user, x = 1;
-		String n1, n2; 
-		double sum, sqrt, nr1 = 0, nr2 = 0;
+		String n1, n2, y; 
+		double sum, sqrt, nr1 = 0, nr2 = 0,  yr = 0;
 		
 		Scanner sc = new Scanner(System.in);
 		
 		while(x == 1) {
-			System.out.println("O que deseja?\n"
+			System.out.println("\nO que deseja?\n"
 					+ "[1]Somar dois números\n"
 					+ "[2]Raiz quadrada de um número");
-			System.out.print("Resposta: ");
+			System.out.print("\nResposta: ");
 			user = sc.nextInt();
 			
 			if (user == 1) {
@@ -46,23 +46,27 @@ public class Main {
 				}
 				
 			}
-			else
-				System.out.println("Opção Inváliada!");
-			System.out.println("\nQuer continuar?\n"
-					+ "[1]SIM\n"
-					+ "[0]NÃO\n"
-					+ "Resposta: ");
-			x = sc.nextInt();
-			
-			if (x == 1 || x == 0) {
-				if (x == 1) {
-					System.out.println("\n");
-				}
-				else 
-					System.out.println("Obrigado por utilizar o código!");
-					x = 0;
-			}
 			else {
+				System.out.println("Opção Inváliada!");
+			}
+			System.out.print("\n\nVoltar ao início?\n"
+			+ "[1]SIM\n"
+			+ "[0]NÃO\n"
+			+ "Resposta: ");
+			y = sc.next();
+			try { 
+				yr = Double.parseDouble(y);
+				if (yr == 0 || yr == 1) {
+					if (yr == 0) {
+						System.out.println("Obrigado por utilizar o código!");
+						x = 0;
+					}
+				}
+				else {
+					System.out.println("Opção invalida");
+				}
+			}catch(NumberFormatException e){
+				System.out.println("Não é um número");
 				System.out.println("Opção inválida!");
 			}
 			
