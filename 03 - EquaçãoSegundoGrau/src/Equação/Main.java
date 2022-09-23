62db5b6937141cef34e87714d4afcbaa9237018d
Erick Gomes <110913912+ErickGms01@users.noreply.github.com>
2022-09-23 20:57:59 -0300

    Questão 3

---
+++ 03 - EquaçãoSegundoGrau/src/Equação/Main.java
@@ -0,0 +1,35 @@
+package Equação;
+
+import java.util.Scanner; 
+
+public class Main {
+
+	public static void main(String[] args) {
+		double a = 0, b, c, delta, x1, x2;
+		
+		Scanner sc = new Scanner(System.in);
+		while (a == 0) {
+			System.out.print("Digite o valor de a: ");
+			a = sc.nextDouble();
+		}
+		System.out.print("Digite o valor de b: ");
+		b = sc.nextDouble();
+		System.out.print("Digite o valor de b: ");
+		c = sc.nextDouble();
+
+		delta = Math.pow(b, 2) - (4*a) * c;
+		if (delta >= 0) {
+			x1 = (-b + Math.sqrt(delta)) / (2 * a);
+			x2 = (-b - Math.sqrt(delta)) / (2 * a);
+			
+			System.out.println("Os valores são: " + x1);
+			System.out.println("Os valores são: " + x2);
+			System.out.println(delta);
+		}
+		else {
+			System.out.println("Inválido");
+		}
+		
+	}
+
+}
