0507520af502da4c3fa16ff43328e71bdfe66e8d
Erick Gomes <110913912+ErickGms01@users.noreply.github.com>
2022-09-27 07:15:41 -0300

    Concertando a formatação

--- 10 - Tabuada/src/tabuada/Main.java
+++ 10 - Tabuada/src/tabuada/Main.java
@@ -6,15 +6,57 @@ import cores.Cores;
 public class Main {
 
 	public static void main(String[] args) {
-		for (int i = 1;i <= 10; i++) {
-			System.out.println(Cores.BLACK + Cores.WHITE_BACKGROUND +  "||=--=--=--=--=--" + Cores.BLUE + Cores.WHITE_BACKGROUND + "Tabuada do " + i + Cores.RESET + Cores.BLACK + Cores.WHITE_BACKGROUND + "--=--=--=--=--=||");
-			for(int j = 1; j <= 10;j++) {
-				System.out.println("||                " + i + " x " + j + " = " + j*i + "               ||");
-			}
-			System.out.println("||=--=--=--=--=--=--=--=--=--=--=--=--=--=--=||" + Cores.RESET );
-			System.out.println();
-		}
+		String formatadoresL = "||=--=--=--=--=--";
+		String formatadoresR = "--=--=--=--=--=||";
+		String formatting = "||=--=--=--=--=--=--=--=--=--=--=--=--=--=--||";
+		String spaceL = "||                ";
+		String spaceR = "                ";
+		String barra = "||";
+		String tabuadaT = "tabuada do ";
 		
+		for (int i = 1;i <= 10; i++) {
+			if (Cores.BLACK.length() + Cores.WHITE_BACKGROUND.length() + formatadoresL.length() + Cores.BLUE.length() + Cores.WHITE_BACKGROUND.length() + tabuadaT.length() + i + Cores.RESET.length() + Cores.BLACK.length() + Cores.WHITE_BACKGROUND.length() + formatadoresR.length() == 95) {
+				formatadoresR = "--=--=--=--=--||";
+				System.out.println(Cores.BLACK + Cores.WHITE_BACKGROUND + formatadoresL + Cores.BLUE + Cores.WHITE_BACKGROUND + "Tabuada do " + i + Cores.RESET + Cores.BLACK + Cores.WHITE_BACKGROUND + formatadoresR);
+				formatadoresR = "--=--=--=--=--=||";
+			}
+			else {
+				System.out.println(Cores.BLACK + Cores.WHITE_BACKGROUND + formatadoresL + Cores.BLUE + Cores.WHITE_BACKGROUND + "Tabuada do " + i + Cores.RESET + Cores.BLACK + Cores.WHITE_BACKGROUND + formatadoresR);
+			}
+			for(int j = 1; j <= 10;j++) {
+				String tab = (spaceL + i + " x " + j + " = " + j*i);
+				if (tab.length() + spaceR.length() + barra.length() == 49) {
+					spaceR = "             ";
+					System.out.println(tab + spaceR + barra);
+					spaceR = "                ";
+				}
+				else if (tab.length() + spaceR.length() + barra.length() == 48 ) {
+					spaceR = "               ";
+					System.out.println(tab + spaceR + barra);
+					spaceR = "                ";
+				}		
+				else if (tab.length() + spaceR.length() + barra.length() == 47) {
+					spaceR = "               ";
+					System.out.println(tab + spaceR + barra);
+					spaceR = "                ";
+				}
+				else if (tab.length() + spaceR.length() + barra.length() == 46) {
+					spaceR = "                ";
+					System.out.println(tab + spaceR + barra);
+					spaceR = "                ";
+				}
+				else if (tab.length() < formatting.length()){
+					spaceR = "                 ";
+					System.out.println(tab + spaceR + barra);
+					spaceR = "                ";
+				}
+				else {
+					
+				}
+			}
+			System.out.println(formatting + Cores.RESET );
+		}
+
 	}
 
-}
+} 
