bc7dce497828cc2876ed3bc34d33ad8a523b5862
Erick Gomes <110913912+ErickGms01@users.noreply.github.com>
2022-10-11 11:41:53 -0300

    modularizing

--- 02 - Opções/src/main/Main.java
+++ 02 - Opções/src/main/Main.java
@@ -1,83 +1,16 @@
 package main;
 
-import java.util.Scanner;
+import util.Util;
 
-import color.Colors;
-	
 public class Main {
 	
 	public static void main(String[] args) {
-		int x = 1, userR;
-		String n1, n2, y, user; 
-		double sum, sqrt, nr1 = 0, nr2 = 0,  yr = 0;
-		
-		Scanner sc = new Scanner(System.in);
-		
+		Util util = new Util();
 		do {
-			System.out.println("\n" + Colors.BLUE +"What do you want?" + Colors.RESET + "\n"
-					+ "[1]Sum 2 numbers\n"
-					+ "[2]Square Root of one number");
-			System.out.print("\nAnswer: ");
-			user = sc.next();
-			try {
-				userR = Integer.parseInt(user);
-				if (userR == 1) {
-					System.out.print("\nEnter a "+ Colors.WHITE_UNDERLINED + "1°" + Colors.RESET + " number: ");
-					n1 = sc.next();
-					System.out.print("Enter a "+ Colors.WHITE_UNDERLINED +  "2°" + Colors.RESET + " number : ");
-					n2 = sc.next();
-					try {
-						nr1 = Double.parseDouble(n1);
-						nr2 = Double.parseDouble(n2);
-						sum = nr1 + nr2;
-						System.out.println("The sum is: " + Colors.YELLOW + Colors.YELLOW_UNDERLINED + sum + Colors.RESET);
-					}catch(NumberFormatException e) {
-						System.out.println(Colors.RED_BOLD + "ERROR!!\n" + Colors.RESET + "This is not a number or this is a real number!");
-					}
-		
-				}
-				else if(userR == 2) {
-					System.out.print("\nEnter a number: ");
-					n1 = sc.next();
-					
-					try {
-						nr1 = Double.parseDouble(n1);
-						sqrt = Math.sqrt(nr1);			
-						System.out.format("The square root of " + n1 + " is: " + Colors.YELLOW + Colors.YELLOW_UNDERLINED + "%.2f" + Colors.RESET, sqrt);
-					}catch(NumberFormatException e) {
-						System.out.println(Colors.RED_BOLD + "ERROR!!\n" + Colors.RESET + "This is not a number or this is a real number!");
-					}
-					
-				}
-				else {
-					System.out.println(Colors.RED_BOLD + "ERROR!!\n" + Colors.RESET + "Invalid Option!!");
-				}
-				System.out.print("\n\nCome back?\n"
-				+ "[1]Yes\n"
-				+ "[0]No\n"
-				+ "Answer: ");
-				y = sc.next();
-				try { 
-					yr = Double.parseDouble(y);
-					if (yr == 0 || yr == 1) {
-						if (yr == 0) {
-							System.out.println(Colors.BLUE + "Thanks for enjoy the code :D" + Colors.RESET);
-							x = 0;
-						}
-					}
-					else {
-						System.out.println(Colors.RED_BOLD + "ERROR!!\n" + Colors.RESET + "Opção invalida");
-					}
-				}catch(NumberFormatException e){
-					System.out.println(Colors.RED_BOLD + "ERROR!!\n" + Colors.RESET + "This is not a number or this is a real number!");
-				}
-				
-			}catch(NumberFormatException e) {
-				System.out.println(Colors.RED_BOLD + "ERROR!!\n" + Colors.RESET + "This is not a number or this is a real number!");
-			}
-			
-		}while(x == 1);	
-			
+			util.InputUser();
+			util.TestInteger();
+			util.repeat();
+		}while (util.x == 1);	
 	}
 
 }
---
+++ 02 - Opções/src/util/Util.java
@@ -0,0 +1,107 @@
+package util;
+
+import java.util.Scanner;
+
+import color.Colors;
+
+public class Util {
+	public int x = 1, userR;
+	public String n1, n2, y, user; 
+	public double sum, sqrt, nr1 = 0 , nr2 = 0, yr = 0;
+	Scanner sc = new Scanner(System.in);
+	public void InputUser() {
+		System.out.println("\n" + Colors.BLUE +"What do you want?" + Colors.RESET + "\n"
+				+ "[1]Sum 2 numbers\n"
+				+ "[2]Square Root of one number");
+		System.out.print("\nAnswer: ");
+		user = sc.next();
+	}
+	
+	public void sumTwoNumbers() {
+		System.out.println("--=--=--=--=--=--=--Sum of two Numbers--=--=--=--=--=--");
+		System.out.print("Enter a "+ Colors.WHITE_UNDERLINED + "1°" + Colors.RESET + " number: ");
+		n1 = sc.next();
+		System.out.print("Enter a "+ Colors.WHITE_UNDERLINED +  "2°" + Colors.RESET + " number : ");
+		n2 = sc.next();
+		try {
+			nr1 = Double.parseDouble(n1);
+			nr2 = Double.parseDouble(n2);
+			sum = nr1 + nr2;
+			System.out.print("The sum is: " + Colors.YELLOW + Colors.YELLOW_UNDERLINED + sum + Colors.RESET);
+		}catch(NumberFormatException e) {
+			Error();
+			sumTwoNumbers();
+		}
+	}
+	
+	public void sqrt() {
+		System.out.println("--=--=--=--=--=--=--Square Root--=--=--=--=--=--");
+		System.out.print("Enter a number: ");
+		n1 = sc.next();
+		
+		try {
+			nr1 = Double.parseDouble(n1);
+			sqrt = Math.sqrt(nr1);			
+			System.out.format("The square root of " + n1 + " is: " + Colors.YELLOW + Colors.YELLOW_UNDERLINED + "%.2f" + Colors.RESET, sqrt);
+		}catch(NumberFormatException e) {
+			Error();
+			sqrt();
+		}
+	}
+	
+	public void TestInteger() {
+		try {
+			userR = Integer.parseInt(user);
+			if (userR == 1) {
+				sumTwoNumbers();
+			}
+			else if(userR == 2) {
+				sqrt();
+			}
+			else {
+				invalidOption();
+				InputUser();
+				TestInteger();
+			}
+			
+		}catch(NumberFormatException e) {
+			Error();
+			InputUser();
+			TestInteger();
+		}
+	}
+	
+	public void repeat() {
+		System.out.print("\n\nCome back?\n"
+		+ "[1]Yes\n"
+		+ "[0]No\n"
+		+ "Answer: ");
+		y = sc.next();
+		try { 
+			yr = Double.parseDouble(y);
+			if (yr == 1) {
+				
+			}	
+			else if (yr == 0){ 
+				System.out.println(Colors.BLUE + "\nThank's for enjoy the code :D" + Colors.RESET);
+				x = 0;
+			}
+			else {
+				invalidOption();
+				repeat();
+			}
+		}catch(NumberFormatException e){
+			Error();
+			repeat();
+		}
+
+	}
+	
+	public void Error() {
+		System.out.println(Colors.RED_BOLD + "\nERROR!!\n" + Colors.RESET + "This is not a number or this is a real number!\n");
+	}
+	
+	public void invalidOption() {
+		System.out.println(Colors.RED_BOLD + "\nERROR!!\n" + Colors.RESET + "Invalid Option!!\n");
+	}
+}
